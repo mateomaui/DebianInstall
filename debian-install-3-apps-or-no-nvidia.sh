9cc848db1bbc93f94d48cc707d6113771d7fb31c
mateomaui <148507115+mateomaui@users.noreply.github.com>
2023-10-20 08:36:17 +0000

    clarified wine repo comment

--- debian-install-3-apps-or-no-nvidia.sh
+++ debian-install-3-apps-or-no-nvidia.sh
@@ -124,7 +124,7 @@ sudo apt update
 # https://www.linuxcapable.com/how-to-install-wine-on-debian-linux/
 # sudo dpkg --add-architecture i386
 curl -fSsL https://dl.winehq.org/wine-builds/winehq.key | gpg --dearmor | sudo tee /usr/share/keyrings/winehq.gpg > /dev/null
-# The line below is commented out, as the "$(lsb_release -cs)" portion returns "faye" on Linux Mint Debian Edition, and there is no wine repo for that name.
+# The line below is commented out, as the "$(lsb_release -cs)" portion returns "faye" on Linux Mint Debian Edition, and there is no winehq.org repo for that name.
 # Until an automated fix is found, just overriding it with a static "bookworm" identifier for Debian 12.
 # echo deb [signed-by=/usr/share/keyrings/winehq.gpg] http://dl.winehq.org/wine-builds/debian/ $(lsb_release -cs) main | sudo tee /etc/apt/sources.list.d/winehq.list
 echo deb [signed-by=/usr/share/keyrings/winehq.gpg] http://dl.winehq.org/wine-builds/debian/ bookworm main | sudo tee /etc/apt/sources.list.d/winehq.list
